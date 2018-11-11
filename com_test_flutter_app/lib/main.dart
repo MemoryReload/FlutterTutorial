9fa8634af74c8de2cf5277432624c551aa0e7c08
MemoryReload <heping_tsdwx@163.com>
2018-11-11 14:58:27 +0800

    This is the very first commit!

---
+++ com_test_flutter_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("EasyList"),
+        ),
+        body: Card(child: Column(children: <Widget>[
+          Image.asset("assets/food.jpg"),
+          Text("Food Paradise")
+        ],),),
+      ),
+    );
+  }
+}
