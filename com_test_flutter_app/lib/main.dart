9ded4ad1ead8d5950c0aa8229f69882663ff842c
MemoryReload <heping_tsdwx@163.com>
2018-11-12 01:17:15 +0800

    some editing to main.dart

--- com_test_flutter_app/lib/main.dart
+++ com_test_flutter_app/lib/main.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 
 void main(List<String> args) => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ["Food Paradise"];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,10 +18,35 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("EasyList"),
         ),
-        body: Card(child: Column(children: <Widget>[
-          Image.asset("assets/food.jpg"),
-          Text("Food Paradise")
-        ],),),
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10),
+              child: RaisedButton(
+                child: Text("Add"),
+                onPressed: () {
+                  print(_products);
+                  setState(() {
+                    _products.add("Advanced Food");
+                  });
+                }),
+              ),
+            Column(
+              children: _products
+                  .map((element) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image(
+                              image: AssetImage("assets/food.jpg"),
+                            ),
+                            Text(element),
+                          ],
+                        ),
+                      ))
+                  .toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
