b5e68369bd1ddedde052f052f1059fabfe677ce3
heping <ping.he@chelaile.net.cn>
2018-11-19 12:10:28 +0800

    change to the control button

--- com_test_flutter_app/lib/products_manager.dart
+++ com_test_flutter_app/lib/products_manager.dart
@@ -4,7 +4,7 @@ import './products.dart';
 class ProductsManager extends StatefulWidget {
   final String startingProduct;
 
-  ProductsManager({this.startingProduct = "Sweets Tester"});
+  ProductsManager({this.startingProduct});
 
   @override
   State<StatefulWidget> createState() {
@@ -17,24 +17,32 @@ class _ProductsManagerState extends State<ProductsManager> {
 
   @override
   void initState() {
-    _products.add(widget.startingProduct);
     super.initState();
+    if (widget.startingProduct != null) {
+      _products.add(widget.startingProduct);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Container(
-          margin: EdgeInsets.all(10),
-          child: RaisedButton(
-            color: Theme.of(context).primaryColor,
-              child: Text("Add"),
-              onPressed: () {
-                setState(() {
-                  _products.add("Advenced food tester");
-                });
-              }),
+        Row(
+          children: <Widget>[
+            Expanded(
+                child: Center(
+                    child: Container(
+              margin: EdgeInsets.symmetric(vertical: 15),
+              child: RaisedButton(
+                  color: Theme.of(context).primaryColor,
+                  child: Text("Add"),
+                  onPressed: () {
+                    setState(() {
+                      _products.add("Advenced food tester");
+                    });
+                  }),
+            ))),
+          ],
         ),
         Products(_products)
       ],
