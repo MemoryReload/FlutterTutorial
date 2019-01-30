54682679c5ce68e524c592d5bcd138546bf6f853
heping <ping.he@chelaile.net.cn>
2019-01-30 17:01:34 +0800

    add a stupid border and corner radius

--- com_test_flutter_app/lib/products.dart
+++ com_test_flutter_app/lib/products.dart
@@ -4,7 +4,7 @@ class Products extends StatelessWidget {
   final List<Map<String, dynamic>> products;
   final Function deleteProduct;
 
-  Products([this.products = const [],this.deleteProduct]);
+  Products([this.products = const [], this.deleteProduct]);
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +16,41 @@ class Products extends StatelessWidget {
                   Image(
                     image: AssetImage(products[index]["image"]),
                   ),
-                  Text(products[index]["title"]),
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(products[index]["title"],
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: "Condensed_Bold")),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).accentColor,
+                              borderRadius: BorderRadius.circular(4)),
+                          child: Text(
+                            "\$${products[index]["price"].toString()}",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                   ButtonBar(
                     alignment: MainAxisAlignment.center,
                     mainAxisSize: MainAxisSize.max,
                     children: <Widget>[
                       FlatButton(
                           child: Text("Details"),
-                          onPressed: () => Navigator.pushNamed(context, "/products/$index")
+                          onPressed: () =>
+                              Navigator.pushNamed(context, "/products/$index")
                                   .then((result) {
                                 if (result) {
                                   deleteProduct(index);
