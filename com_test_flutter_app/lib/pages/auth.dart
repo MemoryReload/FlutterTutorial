e40c25611c4c5b4df572695747c81f1e7e7c0fac
MemoryReload <heping_tsdwx@163.com>
2019-02-11 22:34:36 +0800

    more shit

--- com_test_flutter_app/lib/pages/auth.dart
+++ com_test_flutter_app/lib/pages/auth.dart
@@ -10,6 +10,15 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   String account;
   String password;
+  bool accepted;
+
+  @override
+  void initState() {
+    super.initState();
+    accepted = false;
+    account = "";
+    password = "";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +27,15 @@ class _AuthPageState extends State<AuthPage> {
         title: Text("Login"),
       ),
       body: Container(
-        padding: EdgeInsets.fromLTRB(16, 40, 16, 16),
-        child: Column(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/background.jpg"),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.5), BlendMode.dstATop)),
+        ),
+        padding: EdgeInsets.all(10),
+        child: ListView(
           children: <Widget>[
             TextField(
               autofocus: true,
@@ -43,7 +59,24 @@ class _AuthPageState extends State<AuthPage> {
               },
             ),
             SizedBox(
-              height: 40,
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Accept Terms"),
+                Switch(
+                  value: accepted,
+                  onChanged: (bool value) {
+                    setState(() {
+                      accepted = value;
+                    });
+                  },
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
             ),
             RaisedButton(
               color: Theme.of(context).primaryColor,
