0d006939f23a6f9a199745e3c54a7d79d119a49f
MemoryReload <heping_tsdwx@163.com>
2021-03-13 13:02:54 +0800

    update flutter with configuration change

---
+++ com_test_flutter_app/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/heping/Library/Flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_app"
+export "FLUTTER_TARGET=/Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_app/.dart_tool/package_config.json"
