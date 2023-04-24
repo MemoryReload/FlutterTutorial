22acf3b456e4d08045bfa849ebc717f94c0dc7b4
MemoryReload <heping_tsdwx@163.com>
2023-04-24 21:36:57 +0800

    shop project

---
+++ com_test_flutter_shop/android/app/src/main/kotlin/com/example/com_test_flutter_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.com_test_flutter_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ com_test_flutter_shop/ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/GeneratedPluginRegistrant.d
@@ -0,0 +1,20 @@
+dependencies: \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Modules/module.modulemap \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterEngineGroup.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/heping/Documents/GitHub/FlutterTutorial/com_test_flutter_shop/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/Flutter.h
